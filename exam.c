363ac74d9f49cce0efb1f9589e438dadfc7d0f87
imane_qandyl <102863547+imane-qandyl@users.noreply.github.com>
2024-12-27 20:15:29 +0400

    Update exam.c

--- exam.c
+++ exam.c
@@ -1,67 +1,64 @@
+#include <stdio.h>
 #include <string.h>
-#include <unistd.h>
+#include <sys/types.h>
 #include <sys/wait.h>
-#include <stdlib.h>
+#include <unistd.h>
 
-void err(char *str)
+int err(char *s)
 {
-	while (*str)
-		write(2, str++, 1);
+    while(*s)
+        write(2, s++, 1);
+    return 1;
 }
 
-int cd(char **argv, int i)
+int cd(char **av, int i)
 {
-	if (i != 2)
-		return err("error: cd: bad arguments\n"), 1;
-	if (chdir(argv[1]) < 0)
-		return err("error: cd: cannot change directory to "), err(argv[1]), err("\n"), 1;
-	return 0;
+    if (i != 2)
+        return(err("error: cd: bad arguments\n"));
+    else if (chdir(av[1]) == -1)
+        return (err("error: cd: cannot change directory to "), err(av[1])), err("\n");
+    return 0;
 }
 
-void set_pipe(int has_pipe, int *fd, int end)
+int	execute(char **av, char **envp, int i)
 {
-	if (has_pipe && (dup2(fd[end], end) == -1 || close(fd[0]) == -1 || close(fd[1]) == -1))
-		err("error: fatal\n"), exit(1);
-}
+	int	fd[2];
+	int	status;
 
-int	exec(char **argv, int i, char **envp)
-{
-	int has_pipe, fd[2], pid, status;
-	has_pipe = argv[i] && !strcmp(argv[i], "|");
-
-	if (!has_pipe && !strcmp(*argv, "cd"))
-		return cd(argv, i);
+	int has_pipe = av[i] && !strcmp(av[i], "|");
 	if (has_pipe && pipe(fd) == -1)
-		err("error: fatal\n"), exit(1);
-	if ((pid = fork()) == -1)
-		err("error: fatal\n"), exit(1);
+		return (err("error: fatal\n"));
+	int pid = fork();
 	if (!pid)
 	{
-		argv[i] = 0;
-		set_pipe(has_pipe, fd, 1);
-		if (!strcmp(*argv, "cd"))
-			exit(cd(argv, i));
-		execve(*argv, argv, envp);
-		err("error: cannot execute "), err(*argv), err("\n"), exit(1);
+		av[i] = 0; // Set the element after the pipe or semicolon to null
+		if (has_pipe && (dup2(fd[1], 1) == -1 || close(fd[0]) == -1 || close(fd[1]) == -1))
+			return (err("error: fatal\n"));
+		execve(*av, av, envp);
+		return (err("error: cannot execute "), err(*av), err("\n"));
 	}
 	waitpid(pid, &status, 0);
-	set_pipe(has_pipe, fd, 0);
-	return WIFEXITED(status) && WEXITSTATUS(status);
+	if (has_pipe && (dup2(fd[0], 0) == -1 || close(fd[0]) == -1 || close(fd[1]) == -1))
+		return (err("error: fatal\n"));
+	return (0);
 }
 
-int main(int argc, char **argv, char **envp)
+int	main(int ac, char **av, char **envp)
 {
-	(void)argc;
-	int i = 0, status = 0;
+	int i = 0;
 
-	while (argv[i])
+	if (ac == 1)
+		return (0);
+	while (av[i] && av[++i])
 	{
-		argv += i + 1;
+		av = &av[i];
 		i = 0;
-		while (argv[i] && strcmp(argv[i], "|") && strcmp(argv[i], ";"))
+		while (av[i] && strcmp(av[i], "|") && strcmp(av[i], ";"))
 			i++;
-		if (i)
-			status = exec(argv, i, envp);
+		if (!strcmp(av[0], "cd"))
+			cd(av, i);
+		else if (i)
+			execute(av, envp, i);
 	}
-	return status;
+	return (0);
 }
